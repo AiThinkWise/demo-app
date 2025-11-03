1b4cb767a89317331eade06641f6a3eefaf238b8
vanjerson <118233610+vanjerson@users.noreply.github.com>
2025-11-03 22:58:22 +0800

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/demo-app/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
